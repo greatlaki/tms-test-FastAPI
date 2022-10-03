216a3f10aada3ff2f635e6c4c8f236596f432467
greatlaki <greatlaki911@gmail.com>
2022-10-03 22:16:15 +0300

    the direct connection to db

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,5 @@
-from users import users
-from jobs import jobs
-from base import metadata, engine
+from .users import users
+from .jobs import jobs
+from .base import metadata, engine
 
 metadata.create_all(bind=engine)
