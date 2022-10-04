bcd73fcf14a8d530b342a9e0caf345cf0e3222d3
greatlaki <greatlaki911@gmail.com>
2022-10-04 18:43:22 +0300

    create user's repository

---
+++ repositories/users.py
@@ -0,0 +1,20 @@
+from db.users import users
+from base import BaseRepository
+
+
+class UserRepository(BaseRepository):
+
+    async def get_all(self, limit: int = 100, skip: int = 0):
+        return
+
+    async def get_by_id(self, id: int):
+        return
+
+    async def create(self):
+        return
+
+    async def update(self):
+        return
+
+    async def get_by_email(self, email: str):
+        return
