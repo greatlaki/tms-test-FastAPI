0fdfe85792da0bd13113ae9018e0f0e836181bb8
greatlaki <greatlaki911@gmail.com>
2022-10-04 19:00:58 +0300

    the interface of repository is ready for implementation

--- repositories/users.py
+++ repositories/users.py
@@ -1,20 +1,22 @@
+from typing import List
 from db.users import users
+from models.user import User, UserIn
 from base import BaseRepository
 
 
 class UserRepository(BaseRepository):
 
-    async def get_all(self, limit: int = 100, skip: int = 0):
+    async def get_all(self, limit: int = 100, skip: int = 0) -> List[User]:
         return
 
-    async def get_by_id(self, id: int):
+    async def get_by_id(self, id: int) -> User:
         return
 
-    async def create(self):
+    async def create(self, u: UserIn) -> User:
         return
 
-    async def update(self):
+    async def update(self, u: UserIn) -> User:
         return
 
-    async def get_by_email(self, email: str):
+    async def get_by_email(self, email: str) -> User:
         return
