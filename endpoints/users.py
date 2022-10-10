3bd6f19d753ffd16acc4f3d6bbb995fd37ed1c18
greatlaki <greatlaki911@gmail.com>
2022-10-10 17:49:25 +0300

    updated

--- endpoints/users.py
+++ endpoints/users.py
@@ -12,6 +12,7 @@ async def read_users(
         skip: int = 100):
     """
     The function returns a list of users
+    the object 'depends' takes a function that do some work and returns dependence(our repository)
     limit: maximum number of users
     skip: how many users to skip for pagination
     """
