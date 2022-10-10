32991167eaf862f55086d67a1df2fdcd18c2d91a
greatlaki <greatlaki911@gmail.com>
2022-10-10 20:07:36 +0300

    auth by jwt

--- core/config.py
+++ core/config.py
@@ -3,3 +3,8 @@ from starlette.config import Config
 config = Config(".env")
 
 DATABASE_URL = config("EE_DATABASE_URL", cast=str, default="")
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ALGORITHM = "HS256"
+SECRET_KEY = config("EE_SECRET_KEY",
+                    cast=str,
+                    default="dcb65d39ee02997e933d734911851509f537295ffdc988de273c2837a4cf7b3c")
--- core/security.py
+++ core/security.py
@@ -1,4 +1,7 @@
+import datetime
 from passlib.context import CryptContext
+import jwt
+from core.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -9,3 +12,17 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hash: str) -> bool:
     return pwd_context.verify(password, hash)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    to_encode.update({"exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_access_token(token: str):
+    try:
+        encode_jwt = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    except jwt.JWSError:
+        return None
+    return encode_jwt
